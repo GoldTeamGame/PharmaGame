84b3703965efc058548d00bbe7d38e284ef5de70
TeamGold <TeamGold1819@gmail.com>
2018-10-11 21:11:38 -0500

    Added buttons to hide and show demo

---
+++ Assets/Scripts/Hide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : MonoBehaviour {
+
+    public GameObject obj;
+
+    public void Enable()
+    {
+        obj.SetActive(true);
+    }
+    public void disable()
+    {
+        obj.SetActive(false);
+    }
+}
