4cd047543147b15b6f9bc47448ac8563b5f66ed2
TeamGold <TeamGold1819@gmail.com>
2018-10-16 16:26:49 -0500

    added buttons

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SwitchScene : MonoBehaviour {
+
+    
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
