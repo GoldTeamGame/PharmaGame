0833e9089b1d32e751f01931cbef4a108e039f6f
dylancyphers <dylan.cyphers3@gmail.com>
2018-10-25 16:53:05 -0500

    Added real button and script to hide

---
+++ Assets/Scripts/HideButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideButton : MonoBehaviour
+{
+    Button button;
+
+    // Use this for initialization
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() => Hide());
+    }
+
+    void Hide()
+    {
+        button.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
