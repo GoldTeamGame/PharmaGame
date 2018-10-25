00f0a08df31f7af4000c50e757072b8dc02bc71d
jphearn2 <johearn@siue.edu>
2018-10-25 16:51:43 -0500

    test changes

---
+++ Assets/Scripts/invSetB.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invSetB : MonoBehaviour {
+	static public bool purchaced = false;
+
+	public int test = staticHolder.test;
+
+	public void purchace(){
+		purchaced = true;
+	}
+
+	
+	
+}
---
+++ Assets/Scripts/staticHolder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class staticHolder : MonoBehaviour {
+
+	// Use this for initialization
+
+	public int test = 5;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
